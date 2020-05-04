344b0dccc22af2eead723cd4eca249116e7f8d4c
Jose M. Perez <JMPerez@users.noreply.github.com>
2020-05-04 22:18:48 +0200

    More changes

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -1,6 +1,6 @@
-import assert from "assert";
-import Promise from "promise";
-import PromiseThrottle from "../lib/main.js";
+import assert from 'assert';
+import Promise from 'promise';
+import PromiseThrottle from '../lib/main.js';
 
 function createPromiseThrottle(rps) {
   return new PromiseThrottle({
@@ -9,92 +9,92 @@ function createPromiseThrottle(rps) {
   });
 }
 
-describe("PromiseThrottle", function () {
-  beforeEach(function () {
+describe('PromiseThrottle', function() {
+  beforeEach(function() {
     jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
   });
 
-  describe("#add(fn)", function () {
-    it("should return a promise", function (done) {
+  describe('#add(fn)', function() {
+    it('should return a promise', function(done) {
       var pt10 = createPromiseThrottle(10);
 
-      var fn = function () {
+      var fn = function() {
         return Promise.resolve();
       };
 
       pt10
         .add(fn)
-        .then(function () {
+        .then(function() {
           done();
         })
         .catch(done.fail);
     });
 
-    it("should be resolved with the resolved value of the promise returned by the function", function (done) {
+    it('should be resolved with the resolved value of the promise returned by the function', function(done) {
       var pt10 = createPromiseThrottle(10);
 
-      var fn = function () {
+      var fn = function() {
         return Promise.resolve(42);
       };
 
       pt10
         .add(fn)
-        .then(function (value) {
+        .then(function(value) {
           assert.strictEqual(value, 42);
           done();
         })
         .catch(done.fail);
     });
 
-    it("should be rejected with the rejected error of the promise returned by the function", function (done) {
+    it('should be rejected with the rejected error of the promise returned by the function', function(done) {
       var pt10 = createPromiseThrottle(10);
 
-      var fnError = new Error("Ooops!");
-      var fn = function () {
+      var fnError = new Error('Ooops!');
+      var fn = function() {
         return Promise.reject(fnError);
       };
 
       pt10
         .add(fn)
         .then(done.fail)
-        .catch(function (error) {
+        .catch(function(error) {
           assert.strictEqual(error, fnError);
           done();
         });
     });
 
-    it("should be rejected with the error thrown by the function", function (done) {
+    it('should be rejected with the error thrown by the function', function(done) {
       var pt10 = createPromiseThrottle(10);
 
-      var fnError = new Error("Ooops!");
-      var fn = function () {
+      var fnError = new Error('Ooops!');
+      var fn = function() {
         throw fnError;
       };
 
       pt10
         .add(fn)
         .then(done.fail)
-        .catch(function (error) {
+        .catch(function(error) {
           assert.strictEqual(error, fnError);
           done();
         });
     });
   });
 
-  describe("#addAll([fn1, fn2, ...])", function () {
-    it("should return a promise that is resolved with the proper values", function (done) {
+  describe('#addAll([fn1, fn2, ...])', function() {
+    it('should return a promise that is resolved with the proper values', function(done) {
       var pt10 = createPromiseThrottle(10);
 
-      var fn1 = function () {
+      var fn1 = function() {
         return Promise.resolve(12);
       };
-      var fn2 = function () {
+      var fn2 = function() {
         return Promise.resolve(34);
       };
 
       pt10
         .addAll([fn1, fn2])
-        .then(function (values) {
+        .then(function(values) {
           assert.strictEqual(values[0], 12);
           assert.strictEqual(values[1], 34);
           done();
@@ -102,20 +102,20 @@ describe("PromiseThrottle", function () {
         .catch(done.fail);
     });
 
-    it("should return a promise that is rejected whenever one of the function rejects its promise", function (done) {
+    it('should return a promise that is rejected whenever one of the function rejects its promise', function(done) {
       var pt10 = createPromiseThrottle(10);
 
-      var fn1 = function () {
+      var fn1 = function() {
         return Promise.resolve(12);
       };
-      var fnError = new Error("Ooops!");
-      var fn2 = function () {
+      var fnError = new Error('Ooops!');
+      var fn2 = function() {
         return Promise.resolve(fnError);
       };
 
       pt10
         .addAll([fn1, fn2])
-        .then(function (values) {
+        .then(function(values) {
           assert.strictEqual(values[0], 12);
           assert.strictEqual(values[1], fnError);
           done();
@@ -123,14 +123,14 @@ describe("PromiseThrottle", function () {
         .catch(done.fail);
     });
 
-    it('should throttle properly the function calls, respecting the number of "requestsPerSecond" option', function (done) {
+    it('should throttle properly the function calls, respecting the number of "requestsPerSecond" option', function(done) {
       var pt2 = createPromiseThrottle(2);
 
       var count = 8,
         fns = [],
         resolvedCount = 0,
         resolved = [],
-        fn = function () {
+        fn = function() {
           resolvedCount++;
           return Promise.resolve();
         };
@@ -141,37 +141,37 @@ describe("PromiseThrottle", function () {
 
       pt2.addAll(fns);
 
-      setTimeout(function () {
+      setTimeout(function() {
         resolved.push(resolvedCount);
       }, 700);
 
-      setTimeout(function () {
+      setTimeout(function() {
         resolved.push(resolvedCount);
       }, 1700);
 
-      setTimeout(function () {
+      setTimeout(function() {
         resolved.push(resolvedCount);
       }, 2700);
 
-      setTimeout(function () {
+      setTimeout(function() {
         resolved.push(resolvedCount);
         assert.deepEqual([2, 4, 6, 8], resolved);
         done();
       }, 3700);
     });
 
-    it('should throttle depending on call "weight"', function (done) {
+    it('should throttle depending on call "weight"', function(done) {
       var pt2 = createPromiseThrottle(2),
         begin = new Date();
 
       // By default weight is 1 so after this action delay should be 1/2 sec
-      pt2.add(function () {
+      pt2.add(function() {
         return Promise.resolve();
       });
 
       // This action has weight 2 so delay ater it should be doubled (1 sec)
       pt2.add(
-        function () {
+        function() {
           assert.equal(true, new Date() - begin >= 500);
           return Promise.resolve();
         },
@@ -180,7 +180,7 @@ describe("PromiseThrottle", function () {
 
       // This is more heavy action (weight = 4) so delay should be x4 (2 sec)
       pt2.add(
-        function () {
+        function() {
           assert.equal(true, new Date() - begin >= 1500);
           return Promise.resolve();
         },
@@ -188,42 +188,42 @@ describe("PromiseThrottle", function () {
       );
 
       // This action with default weight. So delay again 1/2 sec
-      pt2.add(function () {
+      pt2.add(function() {
         assert.equal(true, new Date() - begin >= 3500);
         return Promise.resolve();
       });
 
-      pt2.add(function () {
+      pt2.add(function() {
         assert.equal(true, new Date() - begin >= 4000);
         done();
         return Promise.resolve();
       });
     });
 
-    it("should abort promises that have been queued", function (done) {
+    it('should abort promises that have been queued', function(done) {
       var controller = new AbortController();
       var signal = controller.signal;
       var pt = createPromiseThrottle(1);
       var executed = 0;
       var firstPromise = pt.add(
-        function () {
+        function() {
           executed++;
           return Promise.resolve();
         },
         { signal: signal }
       );
       pt.add(
-        function () {
+        function() {
           executed++;
           return Promise.resolve();
         },
         { signal: signal }
-      ).catch(function (e) {
+      ).catch(function(e) {
         assert.equal(1, executed);
         done();
       });
 
-      firstPromise.then(function () {
+      firstPromise.then(function() {
         // calling abort should abort the second promise
         // triggering the `catch` function on the promise returned
         // when adding the second one
@@ -231,66 +231,66 @@ describe("PromiseThrottle", function () {
       });
     });
 
-    it("should abort promises that have been queued (using addAll)", function (done) {
+    it('should abort promises that have been queued (using addAll)', function(done) {
       var controller = new AbortController();
       var signal = controller.signal;
       var pt = createPromiseThrottle(1);
       pt.addAll(
         [
-          function () {
+          function() {
             return Promise.resolve();
           },
-          function () {
+          function() {
             return Promise.resolve();
           },
-          function () {
+          function() {
             return Promise.resolve();
           },
-          function () {
+          function() {
             return Promise.resolve();
           },
         ],
         { signal: signal }
-      ).catch(function (e) {
-        assert.equal("AbortError", e.name);
+      ).catch(function(e) {
+        assert.equal('AbortError', e.name);
         done();
       });
       controller.abort();
     });
 
-    it("should only abort promises that were queued with a signal", function (done) {
+    it('should only abort promises that were queued with a signal', function(done) {
       var controller = new AbortController();
       var signal = controller.signal;
       var pt = createPromiseThrottle(10);
       var results = [];
-      pt.add(function () {
+      pt.add(function() {
         results.push(0);
         return Promise.resolve();
-      }).then(function () {
+      }).then(function() {
         controller.abort();
       });
       pt.add(
-        function () {
+        function() {
           results.push(1);
           return Promise.resolve();
         },
         { signal: signal }
       );
-      pt.add(function () {
+      pt.add(function() {
         results.push(2);
         return Promise.resolve();
       });
       pt.add(
-        function () {
+        function() {
           results.push(3);
           return Promise.resolve();
         },
         { signal: signal }
       );
-      pt.add(function () {
+      pt.add(function() {
         results.push(4);
         return Promise.resolve();
-      }).then(function () {
+      }).then(function() {
         assert.deepEqual([0, 2, 4], results);
         done();
       });
